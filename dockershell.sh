42d988c83cc9b3f859a85ba9c1e611eedea01d15
Anugaan <105033280+Anugaan@users.noreply.github.com>
2022-09-07 08:04:38 +0530

    Update dockershell.sh

--- dockershell.sh
+++ dockershell.sh
@@ -1,6 +1,7 @@
-FROM ubuntu:18.04
-RUN  apt-get update
-RUN  apt-get install -y apache2 curl net-tools
-COPY ./index.html /var/www/html/index.html
-ENTRYPOINT apachectl -D FOREGROUND
-EXPOSE 80
+Docker build image using docker build cmd 
+
+docker login to docker-hub
+docker push the built docker image to registry 
+docker rm the image that is buit locally 
+
+docker run -itd image-that-is-pushed to docker regist
