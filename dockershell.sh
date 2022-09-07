50a634d116b76aa44a6576d7de560cd31d57f4e1
Anugaan <105033280+Anugaan@users.noreply.github.com>
2022-09-07 16:34:20 +0530

    Update dockershell.sh

--- dockershell.sh
+++ dockershell.sh
@@ -1,5 +1,21 @@
 #! /bin/bash
 
- docker run --name ubuntu ubuntu
+docker build .
+apt-get update
+>   apt-get install -y apache2 curl net-tools
+>   service apache2 status
+>   echo 'Hello World' > /var/www/html/index.html
+>   service apache2 start
+>   netstat -plnt
+>   curl localhost
+>   ifconfig
+>   curl 172.17.0.2
+>   exit
+docker run --name ubuntu ubuntu
+docker login -u anugaan
+Password:
+docker push anugaan/myrepo:mytag
+
+
  
  
