28bbb49562f80e6cae9de5c28ccf161c2a7f45a2
Anugaan <105033280+Anugaan@users.noreply.github.com>
2022-09-07 16:20:15 +0530

    Update dockershell.sh

--- dockershell.sh
+++ dockershell.sh
@@ -1,8 +1,5 @@
 #! /bin/bash
 
-
-docker login to docker-hub
-docker push the built docker image to registry 
-docker rm the image that is buit locally 
-
-docker run -itd image-that-is-pushed to docker regist
+ docker run --name ubuntu ubuntu
+ 
+ 
