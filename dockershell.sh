0d9a1922873a9fffb46320dc1d9b379b3dc44482
Anugaan <105033280+Anugaan@users.noreply.github.com>
2022-09-09 08:01:26 +0530

    Update dockershell.sh

--- dockershell.sh
+++ dockershell.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 docker build -t anugaan/myrepo:mytag .
-
+docker image ls 
 docker push anugaan/myrepo:mytag
-docker rm -f mycontianer
+docker rm -f mycontainer
 docker rmi anugaan/myrepo:mytag
 docker ps 
 docker image ls 
-docker rm -f mycontianer
-docker ps -a 
 docker run -itd   -p 8083:80  --name mycontainer anugaan/myrepo:mytag
 docker ps
 
